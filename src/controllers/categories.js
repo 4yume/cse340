b00564ed69a2b95094bce1b072db58a804c74d9f
4yume <ykozuma@byupathway.edu>
2026-09-26 10:44:43 +0900

    create and update category

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -1,6 +1,19 @@
 // Import any needed model functions
-import { getAllCategories, getCategoriesByProjectId, getCategoryById, getProjectsByCategoryId, updateCategoryAssignments } from "../models/categories.js";
+import { getAllCategories, getCategoriesByProjectId, getCategoryById, getProjectsByCategoryId, updateCategoryAssignments, createCategory, updateCategory } from "../models/categories.js";
 import { getProjectDetails } from "../models/projects.js";
+import { body, validationResult } from 'express-validator';
+
+
+// Define validation and sanitization rules for project form
+// Define validation rules for project form
+const categoryValidation = [
+    body('name')
+        .trim()
+        .notEmpty()
+        .withMessage('This is required')
+        .isLength({ min: 3, max: 100 }).withMessage('Name must be between 3 and 100 characters')
+];
+
 
 // Define any controller functions
 const showCategoriesPage = async (req, res) => {
@@ -42,5 +55,70 @@ const processAssignCategoriesForm = async (req, res) => {
     res.redirect(`/project/${projectId}`);
 };
 
+const showNewCategoryForm = async (req, res) => {
+    const title = 'Add new Category';
+
+    res.render('new-category', { title });
+}
+
+const processNewCategoryForm = async (req, res) => {
+    // Check for validation error
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        // Loop through validation errors and flash them
+        errors.array().forEach((error) => {
+            req.flash('error', error.msg);
+        });
+
+        // Redirect back to the new category form
+        return res.redirect('/new-category');
+    }
+
+    // Extract form data from req.body
+    const { name } = req.body;
+
+    // create the new category form in the database
+    const newCategoryId = await createCategory(name);
+
+    // Set a success flash message
+    req.flash('success', 'New category created successfully!');
+    res.redirect(`/category/${newCategoryId}`);
+}
+
+const showEditCategoryForm = async (req, res) => {
+    const categoryId = req.params.id;
+    const categoryDetails = await getCategoryById(categoryId);
+    const title = 'Edit Category';
+
+    res.render('edit-category', { title, categoryDetails });
+};
+
+const processEditCategoryForm = async (req, res) => {
+    // Check for validation errors
+    const results = validationResult(req);
+    if (!results.isEmpty()) {
+        // Validation failded - loop through errors
+        results.array().forEach((error) => {
+            req.flash('error', error.msg);
+        });
+
+        // Redirect back to the edit category form
+        return res.redirect('/edit-category/' + req.params.id);
+    }
+
+    const categoryId = req.params.id;
+    const { name } = req.body;
+
+    await updateCategory(name, categoryId);
+
+    //flash message
+    req.flash('success', 'Category updated successfully!');
+    res.redirect(`/category/${categoryId}`);
+};
+
 // Export any controller functions
-export { showCategoriesPage, showCategoryDetailsPage, showAssignCategoriesForm, processAssignCategoriesForm };
+export {
+    showCategoriesPage, showCategoryDetailsPage, showAssignCategoriesForm, processAssignCategoriesForm, showNewCategoryForm, processNewCategoryForm, categoryValidation,
+    showEditCategoryForm,
+    processEditCategoryForm
+};
--- src/routes.js
+++ src/routes.js
@@ -25,7 +25,12 @@ import {
     showCategoriesPage,
     showCategoryDetailsPage,
     showAssignCategoriesForm,
-    processAssignCategoriesForm
+    processAssignCategoriesForm,
+    showNewCategoryForm,
+    processNewCategoryForm,
+    categoryValidation,
+    showEditCategoryForm,
+    processEditCategoryForm
 } from './controllers/categories.js';
 
 import { testErrorPage } from './controllers/errors.js';
@@ -76,5 +81,15 @@ router.get('/edit-project/:id', showEditProjectForm);
 
 // Route to handle the edit project form submission
 router.post('/edit-project/:id', projectValidation, processEditProjectForm)
+
+// Route for new category page
+router.get('/new-category', showNewCategoryForm);
+// Route to handle new category form submission
+router.post('/new-category', categoryValidation, processNewCategoryForm)
+
+// Route to display the edit category form
+router.get('/edit-category/:id', showEditCategoryForm);
+// Route to handle the edit category form submission
+router.post('/edit-category/:id', categoryValidation, processEditCategoryForm)
 
 export default router;
